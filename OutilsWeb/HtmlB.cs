94a25d5f8dd6680caa990cda548ce7f57143607a
DevMyDrug <baptiste.desbois@outlook.com>
2017-08-25 20:06:34 +0200

    Ajoutez des fichiers projet.

---
+++ OutilsWeb/HtmlB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.HtmlControls;
+
+namespace OutilsWeb
+{
+    public class HtmlB
+    {
+        string id;
+        string classe;
+        string style;
+        HtmlGenericControl balise;
+
+        public void SId(string id)
+        {
+            balise.ID = id;
+        }
+        public void SClass(string classe)
+        {
+            balise.Attributes["class"] = classe;
+        }
+        public void SStyle(string style)
+        {
+            List<string> lStyle = style.Split(';').ToList();
+            foreach(string st in lStyle)
+            {
+                string[] temp = st.Split(',');
+                balise.Style.Add(temp[0], temp[1]);
+            }
+        }
+
+        public HtmlB(string balise)
+        {
+            this.balise = new HtmlGenericControl(balise);
+        }
+
+        public void AddChilds(HtmlB child)
+        {
+            balise.Controls.Add(child.GetHtml());
+        }
+
+        public HtmlGenericControl GetHtml()
+        {
+            return balise;
+        }
+
+    }
+}
